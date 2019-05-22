ff1d3e78814811e866a2f0ca961c423b53f2e660
KLR <prefix614@gmail.com>
2019-05-22 15:28:37 +0100

    An example to set hoverfly as a proxy

---
+++ src/test/java/io/klr/ci/service/ExternalHoverflyTimingServiceTest.java
@@ -0,0 +1,27 @@
+package io.klr.ci.service;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExternalHoverflyTimingServiceTest {
+
+    private TimingService ts;
+
+    @Before
+    public void init() {
+        ts = new TimingService();
+        ts.setUrl("http://time.jsontest.com/");
+        System.setProperty("proxySet", String.valueOf(true));
+        System.setProperty("http.proxyHost", "localhost");
+        System.setProperty("http.proxyPort", String.valueOf(8500));
+    }
+
+    @Test
+    public void getNormalResponse() {
+        final String time = ts.invoke();
+        assertThat(time, equalTo("05-22-2019 07:33:19 AM"));
+    }
+}
